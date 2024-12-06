59ad807ac8ab43d56801705129eef429156cbe39
情野陸 <rikuseino33@gmail.com>
2024-12-06 20:03:45 +0900

    feat: hostの設定&CORS許可

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path';
 
 export default ({ mode }) => {
-  const env = loadEnv(mode, resolve(__dirname, '..'));
+  const env = loadEnv(mode, process.cwd());
 
   return defineConfig({
     server: {
+      host: env.VITE_HOST,
       port: env.VITE_PORT,
       watch: {
         usePolling: true
       },
       proxy: {
         '/api/greeting': {
-          target: `${env.VITE_HOST}:${env.VITE_API_PORT}`,
+          target: `http://${env.VITE_HOST}:${env.VITE_API_PORT}`,
           changeOrigin: true,
           secure: false,
         },

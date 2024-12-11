c79013b53dda1bf4d49308feeedc9c3ef43a1fe8
RIKU-SEINO <rikuseino33@gmail.com>
2024-12-12 00:24:44 +0900

    feat: DB接続テスト用のエンドポイントに変更

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const cors = require('cors');
-const { testConnection } = require('./config/db');
+const { Qrcode } = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,7 +12,14 @@ app.get('/api/greet', async (req, res) => {
   res.json({ message: 'Hello world, Backend!' });
 });
 
-app.get('/api/db', testConnection);
+app.get('/api/qrcodes', async (req, res) => {
+  try {
+    const qrcodes = await Qrcode.findAll();
+    res.json(qrcodes);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);

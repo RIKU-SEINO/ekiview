4bdcd34556bac20b82e7567680e44e0da7ad7906
RIKU-SEINO <rikuseino33@gmail.com>
2024-12-12 00:26:29 +0900

    feat: dbの接続情報を書き換える

---
+++ backend/config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config('../.env');
+
+module.exports = {
+  development: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DB,
+    host: process.env.PG_HOST,
+    dialect: 'postgres',
+    port: process.env.PG_PORT,
+    timezone: process.env.TZ || 'Asia/Tokyo'
+  },
+  production: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DB,
+    host: process.env.PG_HOST,
+    dialect: 'postgres',
+    port: process.env.PG_PORT,
+    timezone: process.env.TZ || 'Asia/Tokyo'
+  },
+  test: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DB,
+    host: process.env.PG_HOST,
+    dialect: 'postgres',
+    port: process.env.PG_PORT,
+    timezone: process.env.TZ || 'Asia/Tokyo'
+  },
+};

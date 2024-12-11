88adcc9b7ee8079d3c95c0f8b81acb39345da862
RIKU-SEINO <rikuseino33@gmail.com>
2024-12-12 02:22:51 +0900

    feat: Express Appの作成をapp.jsに切り分けた

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,7 @@
+const app = require('./app');
 require('dotenv').config({ path: '../.env' });
-const express = require('express');
-const cors = require('cors');
-const { Qrcode } = require('./models');
 
-const app = express();
 const PORT = process.env.PORT || 5000;
-
-app.use(cors());
-
-app.get('/api/greet', async (req, res) => {
-  res.json({ message: 'Hello world, Backend!' });
-});
-
-app.get('/api/qrcodes', async (req, res) => {
-  try {
-    const qrcodes = await Qrcode.findAll();
-    res.json(qrcodes);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);

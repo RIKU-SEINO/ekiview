5f818139ed527886603f194955ae86165e27e968
RIKU-SEINO <rikuseino33@gmail.com>
2024-12-12 02:24:30 +0900

    feat: ルーティングの骨組み作成

---
+++ backend/routes/qrcodeRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const qrcodeController = require('../controllers/qrcodeController');
+
+// TODO: QRコード処理に関するエンドポイントへのルーティングを実装
+
+module.exports = router;
---
+++ backend/routes/routeSearchRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const routeSearchController = require('../controllers/routeSearchController');
+
+// TODO: 経路検索を行うエンドポイントへのルーティングを実装
+
+module.exports = router;

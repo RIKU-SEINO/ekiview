beea3c1792cf472670ba4239a9a83a144454540f
RIKU-SEINO <rikuseino33@gmail.com>
2024-12-15 03:39:33 +0900

    feat: 経路検索を行うエンドポイントのルーティングを設定

--- backend/routes/routeSearchRoutes.js
+++ backend/routes/routeSearchRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const routeSearchController = require('../controllers/routeSearchController');
 
-// TODO: 経路検索を行うエンドポイントへのルーティングを実装
-
 module.exports = router;
+
+router.get('/search', routeSearchController.routeSearch);

ef36d088d1cab9c793155c6793fb98a4da22d4b1
RIKU-SEINO <rikuseino33@gmail.com>
2024-12-17 20:05:39 +0900

    feat: 経路検索結果画面の大元となるUIを実装

---
+++ frontend/src/pages/RouteResultPage.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Button from "../components/Button";
+import StreetView from "../partials/StreetViewPartial";
+import RouteInformationPartial from "../partials/RouteInformationPartial";
+
+const RouteResultPage = () => {
+  const location = useLocation();
+  const { results, originPanorama } = location.state;
+  const [activeView, setActiveView] = useState("streetView");
+
+  const switchToStreetView = () => {
+    setActiveView("streetView");
+  };
+  const switchToRouteInformation = () => {
+    setActiveView("routeInformation");
+  };
+
+  return (
+    <div style={styles.page}>
+      {/* Header */}
+      <Header title="EkiView - Route Result" />
+
+      {/* Main Content */}
+      <div style={styles.mainContent}>
+        <div style={styles.btnContainerFlex}>
+          <Button
+            text="Street View"
+            onClick={switchToStreetView}
+            style={{
+              ...styles.button,
+              borderColor: activeView === "streetView" ? "black" : "white",
+              outline: activeView === "streetView" ? "1.5px solid black" : "0.2px solid gray",
+            }}
+          />
+          <Button
+            text="Route Information"
+            onClick={switchToRouteInformation}
+            style={{
+              ...styles.button,
+              borderColor: activeView === "routeInformation" ? "black" : "white",
+              outline: activeView === "routeInformation" ? "1.5px solid black" : "0.2px solid gray",
+            }}
+          />
+        </div>
+        { activeView === "streetView" ? (
+          <StreetView results={results} originPanorama={originPanorama} />
+        ) : (
+          <RouteInformationPartial results={results} />
+        )}
+      </div>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
+
+const styles = {
+  page: {
+    fontFamily: "Arial, sans-serif",
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh",
+  },
+  mainContent: {
+    flex: 1,
+    padding: "20px",
+    textAlign: "center",
+    marginTop: "60px",
+  },
+  btnContainerFlex: {
+    height: "70px",
+    display: "flex",
+    justifyContent: "center",
+    marginBottom: "20px",
+    gap: "10px",
+  },
+  button: {
+    fontSize: "13px",
+    cursor: "pointer",
+    color: "black",
+    border: "2px solid white",
+    padding: "10px 20px",
+    borderRadius: "5px",
+    backgroundColor: "white",
+  },
+};
+
+export default RouteResultPage;

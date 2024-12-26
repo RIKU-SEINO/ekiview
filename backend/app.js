9d10ccd38c0ff91b1f89e64a930697b52eb11515
Daiikari <ikada1811@outlook.jp>
2024-12-26 16:33:15 +0900

    feat:場所検索のルーティング周りの実装

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', qrcodeRoutes);
-app.use('/api', placeSearchRoutes);
+app.use('/api/placesearch', placeSearchRoutes);
 app.use('/api/routes', routeSearchRoutes);
 
 app.get('/api/greet', async (req, res) => {

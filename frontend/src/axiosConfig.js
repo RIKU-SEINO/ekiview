57ab5a761c11a92764bd378f2fab143d52d701f4
RIKU-SEINO <rikuseino33@gmail.com>
2025-01-18 22:24:00 +0900

    fix: loggingを追加

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 console.log('VITE_API_BASE_URL:', __VITE_API_BASE_URL__);
+console.log('VITE_API_BASE_URL_STRING:', JSON.stringify(process.env.VITE_API_BASE_URL));
 
 const axiosInstance = axios.create({
-  baseURL: __VITE_API_BASE_URL__,
+  baseURL: process.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 export default axiosInstance;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -25,7 +25,7 @@ export default ({ mode }) => {
       include: ['react-zxing'],
     },
     define: {
-      __VITE_API_BASE_URL__: JSON.stringify(process.env.VITE_API_BASE_URL),
+      'process.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),
     },
   });
 };

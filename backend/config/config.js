39c508c1af6ea22c4897c8f4c6d68a54cd63d765
RIKU-SEINO <rikuseino33@gmail.com>
2025-01-18 16:02:36 +0900

    fix: ローカル環境ではhostとportを明示的に指定

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,21 +3,27 @@ module.exports = {
     username: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DB,
+    host: process.env.PG_HOST,
     dialect: 'postgres',
+    port: process.env.PG_PORT,
     timezone: process.env.TZ || 'Asia/Tokyo'
   },
   production: {
     username: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DB,
+    host: process.env.PG_HOST,
     dialect: 'postgres',
+    port: process.env.PG_PORT,
     timezone: process.env.TZ || 'Asia/Tokyo'
   },
   test: {
     username: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DB,
+    host: process.env.PG_HOST,
     dialect: 'postgres',
+    port: process.env.PG_PORT,
     timezone: process.env.TZ || 'Asia/Tokyo'
   },
 };

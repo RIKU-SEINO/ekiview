bf82afd0e1f035eb669f1715446ffcf1d9178eae
RIKU-SEINO <rikuseino33@gmail.com>
2025-01-19 17:12:55 +0900

    feat: 不要な環境変数を削除

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,7 +6,6 @@ module.exports = {
     host: process.env.PG_HOST,
     dialect: 'postgres',
     port: process.env.PG_PORT,
-    timezone: process.env.TZ || 'Asia/Tokyo'
   },
   production: {
     username: process.env.PG_USER,
@@ -14,14 +13,5 @@ module.exports = {
     database: process.env.PG_DB,
     host: process.env.PG_HOST || process.env.PG_URL.split('@')[1].split('/')[0],
     dialect: 'postgres',
-  },
-  test: {
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DB,
-    host: process.env.PG_HOST,
-    dialect: 'postgres',
-    port: process.env.PG_PORT,
-    timezone: process.env.TZ || 'Asia/Tokyo'
   },
 };
